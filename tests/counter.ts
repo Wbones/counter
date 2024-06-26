f5be696ae7161c37bb2c61c56f920c3f75843951
Wbones <vanessa@MacBook-Air-de-Vanessa.local>
2024-06-26 10:14:45 -0500

    second commit

--- tests/counter.ts
+++ tests/counter.ts
@@ -1,16 +1,32 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Counter } from "../target/types/counter";
+import { assert } from "chai";
+import { BN } from "bn.js";
 
 describe("counter", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env()
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Counter as Program<Counter>;
+  
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+  it("should create a counter", async () => {
+    const count = new BN(0);
+    const counterKp = new anchor.web3.Keypair();
+    
+    const tx = await program.methods.createCounter(count).accounts({
+      counter: counterKp.publicKey,
+      authority: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId
+    })
+    .signers([counterKp])
+    .rpc();
     console.log("Your transaction signature", tx);
+
+    const counter = await program.account.counter.fetch(counterKp.publicKey);
+    console.log("counter count is:", counter.count.toNumber());
+    assert(count.eq(counter.count));
   });
 });
